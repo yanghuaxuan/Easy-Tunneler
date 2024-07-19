126eaea2dbfd9ab8b786c97aae98bd4996da77fc
yanghuaxuan <yanghuaxuan@gmail.com>
2024-07-18 19:12:03 -0700

    cap width for design

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,14 +10,14 @@ const route = useRoute()
 <template>
   <v-layout>
     <v-main>
-      <v-row v-if="route.name === 'tunnels'" class="d-flex align-center">
-        <v-col>
-          <h1 class="text-h3">Tunnels</h1>
-        </v-col>
-        <v-col class="d-flex justify-end">
-          <v-btn @click="$router.replace({ name: 'settings' })" class="rounded-button" size="x-large" variant="text"
-            :icon="mdiWrenchOutline" />
-        </v-col>
+      <v-row v-if="route.name === 'tunnels'">
+      <v-col>
+        <h1 class="text-h3">Tunnels</h1>
+      </v-col>
+      <v-col class="d-flex justify-end">
+        <v-btn @click="$router.replace({ name: 'settings' })" class="rounded-button" size="x-large" variant="text"
+          :icon="mdiWrenchOutline" />
+      </v-col>
       </v-row>
       <v-row v-else class="d-flex align-center">
         <v-col>

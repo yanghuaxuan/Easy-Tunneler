d3051a07ac9e2b9096e30655b0cb1d3846748480
yanghuaxuan <yanghuaxuan@gmail.com>
2024-07-20 21:13:44 -0700

    Bald

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,14 +10,14 @@ const route = useRoute()
 <template>
   <v-layout>
     <v-main>
-      <v-row v-if="route.name === 'tunnels'">
-      <v-col>
-        <h1 class="text-h3">Tunnels</h1>
-      </v-col>
-      <v-col class="d-flex justify-end">
-        <v-btn @click="$router.replace({ name: 'settings' })" class="rounded-button" size="x-large" variant="text"
-          :icon="mdiWrenchOutline" />
-      </v-col>
+      <v-row class="d-flex align-center" v-if="route.name === 'tunnels'">
+        <v-col>
+          <h1 class="header">Tunnels</h1>
+        </v-col>
+        <v-col class="d-flex justify-end">
+          <v-btn @click="$router.replace({ name: 'settings' })" class="rounded-button" size="x-large" variant="text"
+            :icon="mdiWrenchOutline" />
+        </v-col>
       </v-row>
       <v-row v-else class="d-flex align-center">
         <v-col>
@@ -25,7 +25,7 @@ const route = useRoute()
             :icon="mdiArrowLeft" />
         </v-col>
         <v-col class="d-flex justify-end">
-          <h1 class="text-h3">Settings</h1>
+          <h1 class="header">Settings</h1>
         </v-col>
       </v-row>
       <!-- <RouterView v-slot="{ Component }">
@@ -39,6 +39,10 @@ const route = useRoute()
 </template>
 
 <style>
+.header {
+  font-size: 55px;
+}
+
 .slider-enter-active,
 .slider-leave-active {
   transition: transform 0.25s;
